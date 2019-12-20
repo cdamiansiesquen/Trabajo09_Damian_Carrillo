df1ff2fb73dc8f13a450d8e512096c2703edf15c
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-12-20 11:05:21 -0500

    Se ha terminado

---
+++ lote.bat
@@ -0,0 +1,148 @@
+python Damian\app1.py 10 15
+python Damian\app1.py 5 4
+python Damian\app1.py 50 5
+
+
+
+python Damian\app2.py 15 75 45 65
+python Damian\app2.py 1 4 9 0
+python Damian\app2.py 1 1 1 -1
+
+
+
+python Damian\app3.py 201
+python Damian\app3.py 2019
+python Damian\app3.py Hola
+
+
+
+python Damian\app4.py 1.2 1.199
+python Damian\app4.py 1.2 4
+python Damian\app4.py 1.2 1.2
+
+
+
+python Damian\app5.py Alexis
+python Damian\app5.py 12548
+python Damian\app5.py Petroperu
+
+
+
+python Damian\app6.py 1017546620
+python Damian\app6.py 10175466201
+python Damian\app6.py 1234
+
+
+
+python Damian\app7.py 72507717
+python Damian\app7.py 123456h4
+python Damian\app7.py 12
+
+
+
+python Damian\app8.py 121
+python Damian\app8.py hola
+python Damian\app8.py 1234
+
+
+
+python Damian\app9.py 4
+python Damian\app9.py 5
+python Damian\app9.py 45
+
+
+
+python Damian\app10.py 42 10
+python Damian\app10.py 0 2
+python Damian\app10.py 0 0
+
+
+
+python Damian\app11.py Juaan
+python Damian\app11.py 12234
+python Damian\app11.py alexisds
+
+
+
+python Damian\app12.py 123456789012
+python Damian\app12.py hola
+python Damian\app12.py 1234567890123
+
+
+
+python Damian\app13.py 4 7
+python Damian\app13.py 5 6
+python Damian\app13.py 0 1
+
+
+
+python Damian\app14.py 7
+python Damian\app14.py 2
+python Damian\app14.py 5
+
+
+
+python Damian\app15.py 15 15
+python Damian\app15.py 15 14
+python Damian\app15.py 15 2
+
+
+
+python Damian\app16.py holaaa
+python Damian\app16.py 145
+python Damian\app16.py AON
+
+
+
+python Damian\app17.py 7 8
+python Damian\app17.py 1 0
+python Damian\app17.py 5 6
+
+
+
+python Damian\app18.py 50 5 5
+python Damian\app18.py 40 2 4
+python Damian\app18.py 10 5 6
+
+
+
+python Damian\app19.py 5 6 7
+python Damian\app19.py 1 0 1
+python Damian\app19.py 45 85 78
+
+
+
+python Damian\app20.py 20 20
+python Damian\app20.py 5 7
+python Damian\app20.py 100 40
+
+
+
+python Damian\app21.py m6q457
+python Damian\app21.py m6q4574
+python Damian\app21.py MQ4W87
+
+
+
+python Damian\app22.py 979995478
+python Damian\app22.py Hola
+python Damian\app22.py 97874544h
+
+
+
+python Damian\app23.py 40 5
+python Damian\app23.py 10 14
+python Damian\app23.py 9 9
+
+
+
+python Damian\app24.py ANOUS34
+python Damian\app24.py ANOUS3
+python Damian\app24.py ANOUSS
+
+
+
+python Damian\app25.py 4 8 12
+python Damian\app25.py 10 15 2
+python Damian\app25.py 9 9 8
+
